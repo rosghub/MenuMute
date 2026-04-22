9b03673e343655cabf530313c18a2fdedb674735
brandon@macos <brosini9@gmail.com>
2026-04-22 01:57:46 -0400

    Initial Commit

---
+++ MenuMute/MenuMuteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMuteApp.swift
+//  MenuMute
+//
+//  Created by Brandon on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MenuMuteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
